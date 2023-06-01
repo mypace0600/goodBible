9f2973abcad95b269a82082eb6cb0830574f4eb7
mypace0600 <mypace0600@gmail.com>
2023-06-01 22:47:50 +0900

    text save function

--- lib/widgets/content_widget.dart
+++ lib/widgets/content_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ContentWidget extends StatelessWidget {
   final String book, text;
@@ -12,14 +13,27 @@ class ContentWidget extends StatelessWidget {
     required this.text,
   });
 
+  Future<void> saveTextAddressToLocalStorage(
+      String book, int chapter, int verse, String text) async {
+    String address = '$book ${chapter + 1} $verse';
+    print(address);
+    const storage = FlutterSecureStorage();
+    await storage.write(key: address, value: text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Flexible(
-          child: Text(
-            ':$verse $text',
-            softWrap: true,
+          child: GestureDetector(
+            onLongPress: () {
+              saveTextAddressToLocalStorage(book, chapter, verse, text);
+            },
+            child: Text(
+              ':$verse $text',
+              softWrap: true,
+            ),
           ),
         ),
       ],
