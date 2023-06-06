7e348671e0b9e4976b3ce561e769dc259936cd55
mypace0600 <mypace0600@gmail.com>
2023-06-07 07:23:24 +0900

    firebase and google login

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,7 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AboutScreen extends StatelessWidget {
+class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
+
+  @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _user = FirebaseAuth.instance.currentUser;
+  }
+
+  void settingNotification() {}
+
+  void shareApp() {}
+
+  void loginAndOut() {}
 
   @override
   Widget build(BuildContext context) {
@@ -24,30 +44,35 @@ class AboutScreen extends StatelessWidget {
               child: Row(
                 children: [
                   Container(
-                    margin: const EdgeInsets.fromLTRB(
-                      0,
-                      0,
-                      20,
-                      0,
-                    ),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.black,
-                    ),
-                    child: const Text('img'),
-                  ),
-                  const Column(
+                      margin: const EdgeInsets.fromLTRB(
+                        0,
+                        0,
+                        20,
+                        0,
+                      ),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.black,
+                      ),
+                      child: _user != null
+                          ? Image.network(_user!.photoURL ?? "")
+                          : const Text("")),
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('email'),
+                        padding: const EdgeInsets.all(8.0),
+                        child: _user != null
+                            ? Text('${_user!.email}')
+                            : const Text('email'),
                       ),
                       Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('name'),
+                        padding: const EdgeInsets.all(8.0),
+                        child: _user != null
+                            ? Text('${_user!.displayName}')
+                            : const Text('name'),
                       ),
                     ],
                   ),
@@ -64,60 +89,76 @@ class AboutScreen extends StatelessWidget {
                     BoxDecoration(borderRadius: BorderRadius.circular(50)),
                 child: Column(
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: BorderDirectional(
-                          bottom: BorderSide(
-                            color: Colors.black.withOpacity(0.1),
-                            width: 1,
+                    GestureDetector(
+                      onTap: settingNotification,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: BorderDirectional(
+                            bottom: BorderSide(
+                              color: Colors.black.withOpacity(0.1),
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 15,
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.notifications_outlined),
+                              Text('알림'),
+                            ],
                           ),
                         ),
                       ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 20,
-                          horizontal: 15,
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.notifications_outlined),
-                            Text('알림'),
-                          ],
-                        ),
-                      ),
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: BorderDirectional(
-                          bottom: BorderSide(
-                            color: Colors.black.withOpacity(0.1),
-                            width: 1,
+                    GestureDetector(
+                      onTap: shareApp,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: BorderDirectional(
+                            bottom: BorderSide(
+                              color: Colors.black.withOpacity(0.1),
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 15,
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.share_outlined),
+                              Text('공유'),
+                            ],
                           ),
                         ),
                       ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
+                    ),
+                    GestureDetector(
+                      onTap: loginAndOut,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
                           vertical: 20,
                           horizontal: 15,
                         ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.share_outlined),
-                            Text('공유'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 15,
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(Icons.logout_outlined),
-                          Text('로그아웃'),
-                        ],
+                        child: _user != null
+                            ? const Row(
+                                children: [
+                                  Icon(Icons.logout_outlined),
+                                  Text('로그아웃'),
+                                ],
+                              )
+                            : const Row(
+                                children: [
+                                  Icon(Icons.login_outlined),
+                                  Text('로그인'),
+                                ],
+                              ),
                       ),
                     ),
                   ],
